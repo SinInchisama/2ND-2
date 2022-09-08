aab97b1bd82a942af35542b88b80c44ed6996536
SinInchisama <sun98390123@gmail.com>
2022-09-09 03:47:14 +0900

    컴그0-4 create

--- 컴퓨터 그래픽스/과제/워밍업/Computer Graphic 0-4/Computer Graphic 0-4/소스.c
+++ 컴퓨터 그래픽스/과제/워밍업/Computer Graphic 0-4/Computer Graphic 0-4/소스.c
@@ -5,23 +5,69 @@ typedef struct Rectangle {
 	int x, y;
 }Rectangle;
 
-void initxy(Rectangle* rect[]);
+void initxy(Rectangle rect[]);
+void crashcheck(Rectangle rect[]);
 
 int main() {
 
 	Rectangle rectangle[2];
+	int round = 0;
+	char c = 0;
+
 	srand(time(NULL));
 
 	initxy(rectangle);
 
-	while (1) {
+	while (c != 'q') {
+		printf("Rect 1 : (%d, %d) (%d, %d)\n", rectangle[0].x, rectangle[0].y, rectangle[0].x + 100, rectangle[0].y + 100);
+		printf("Rect 2 : (%d, %d) (%d, %d)\n", rectangle[1].x, rectangle[1].y, rectangle[1].x + 100, rectangle[1].y + 100);
+		printf("order : %d \n", round);
+		scanf(" %c", &c);
 
+		switch (c) {
+		case 'w':
+			if (rectangle[round].y - 30 >= 0)
+				rectangle[round].y = rectangle[round].y - 30;
+			break;
+		case 's':
+			if (rectangle[round].y + 130 <= 600)
+				rectangle[round].y = rectangle[round].y + 30;
+			break;
+		case 'd':
+			if (rectangle[round].x + 130 <= 800)
+				rectangle[round].x = rectangle[round].x + 30;
+			break;
+		case 'a':
+			if (rectangle[round].x - 30 >= 0)
+				rectangle[round].x = rectangle[round].x - 30;
+			break;
+		case 'q':
+			break;
+		}
+
+		crashcheck(rectangle);
+
+		if (!round)
+			round = 1;
+		else
+			round = 0;
 	}
 }
 
 void initxy(Rectangle rect[]) {
 	for (int i = 0; i < 2; i++) {
-		rect[i].x = rand() % 800;
-		rect[i]->y = rand() % 600;
+		rect[i].x = rand() % 701;
+		rect[i].y = rand() % 501;
 	}
+}
+
+void crashcheck(Rectangle rect[]) {
+	if (rect[0].x <= rect[1].x + 100&& rect[0].x >= rect[1].x && rect[0].y <= rect[1].y + 100&& rect[0].y >= rect[1].y)
+		printf("Creat crash \n");
+	else if(rect[0].x+100>=rect[1].x&& rect[0].x + 100 <= rect[1].x + 100 &&rect[0].y<=rect[1].y+100&& rect[0].y >= rect[1].y)
+		printf("Creat crash \n");
+	else if(rect[0].x+100>=rect[1].x&& rect[0].x + 100 <= rect[1].x + 100 &&rect[0].y+100>=rect[1].y && rect[0].y + 100 <= rect[1].y + 100)
+		printf("Creat crash \n");
+	else if(rect[0].x<=rect[1].x + 100 && rect[0].x >= rect[1].x &&rect[0].y+100>=rect[1].y && rect[0].y + 100 <= rect[1].y + 100)
+		printf("Creat crash \n");
 }
